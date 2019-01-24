376e013ef979ec648af456e5bd62f405f55cf524
SparksFullStack <csparks@antioch.edu>
2019-01-24 15:17:11 -0800

    added a getter and a setter method on the model class

--- js/classes/modelControllers/model.js
+++ js/classes/modelControllers/model.js
@@ -1,5 +1,13 @@
 class Model {
     constructor(){
-        this.score = 0;
+        this._score = 0;
+    }
+
+    set score(val){
+        this._score = val;
+    }
+
+    get score(){
+        return this._score;
     }
 }
